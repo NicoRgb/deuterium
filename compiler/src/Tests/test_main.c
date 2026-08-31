92a0ae3b1152731a011b492dcbefb670aa0813ae
Malediktus <malediktusrgb@gmail.com>
2026-09-01 00:51:36 +0200

    fix: added dummy get_global_config to test_main so test framework compiles

--- compiler/src/Tests/test_main.c
+++ compiler/src/Tests/test_main.c
@@ -1,5 +1,19 @@
 #include <testing.h>
 
+#include "arguments.h"
+
+compiler_config_t g_config;
+
+compiler_config_t *get_global_config(void)
+{
+    g_config.mode = COMPILER_MODE_COMPILE;
+    g_config.verbosity = COMPILER_VERBOSITY_ALL;
+    g_config.outfile = NULL;
+    g_config.infiles = NULL_ARRAY;
+
+    return &g_config;
+}
+
 int main(void)
 {
     run_tests();
